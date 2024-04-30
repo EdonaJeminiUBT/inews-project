e79d67bef38afa1f1ec8d8aabf3b0636eecc0f3e
ArbenitUBT <ab47053@ubt-uni.net>
2024-04-30 23:49:27 +0200

    Added names for the pages

--- frontend/src/pages/Celebrity.tsx
+++ frontend/src/pages/Celebrity.tsx
@@ -5,6 +5,7 @@ export function Celebrity(){
         <>
          <div className="news-feed">
                 <div className="news-btns">
+                <h1>Celebrity</h1>
                 <button className="like-btn">Like</button>
                 </div>
             </div>
