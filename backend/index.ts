f2d4acf82032d3f7d9f79b6ee1b2dbfafaa0e7f1
ArbenitUBT <ab47053@ubt-uni.net>
2024-05-06 15:17:01 +0200

    Changed the users and news database from mongo to mysql

--- backend/index.ts
+++ backend/index.ts
@@ -1,32 +1,29 @@
 import express, { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { client } from './db/mongo';
+import { connection, connectMySQL } from './db/mysql';
 import { SignInData, SignUpData } from './types';
 import cors from 'cors'; 
-
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+connectMySQL();
 
-// Sign up route
 app.post('/signup', async (req: Request, res: Response) => {
   const { email, password, name } = req.body as SignUpData;
 
   try {
-    const db = client.db('inews');
-    const usersCollection = db.collection('users');
-
-    const existingUser = await usersCollection.findOne({ email });
-    if (existingUser) {
+    const [rows] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+    if (Array.isArray(rows) && rows.length > 0) {
       return res.status(400).send('Email address already exists');
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await usersCollection.insertOne({ email, password: hashedPassword, name });
+    await connection.query('INSERT INTO users (email, password, name) VALUES (?, ?, ?)', [email, hashedPassword, name]);
 
     res.status(201).send('User created successfully');
   } catch (error) {
@@ -35,32 +32,23 @@ app.post('/signup', async (req: Request, res: Response) => {
   }
 });
 
-
-
-// Sign in route
 app.post('/signin', async (req: Request, res: Response) => {
   const { email, password } = req.body as SignInData;
 
   try {
-    // Access the MongoDB database and collection
-    const db = client.db('inews');
-    const usersCollection = db.collection('users');
-
-    // Find user in the collection
-    const user = await usersCollection.findOne({ email });
+    const [rows] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+    const user = rows[0];
     
     if (!user) {
       return res.status(401).send('Invalid email or password');
     }
 
-    // Compare passwords
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return res.status(401).send('Invalid email or password');
     }
 
-    // Generate JWT token
-    const token = jwt.sign({ userId: user._id }, 'secret', { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user.id }, 'secret', { expiresIn: '1h' });
 
     res.send({ token });
   } catch (error) {
@@ -68,6 +56,7 @@ app.post('/signin', async (req: Request, res: Response) => {
     res.status(500).send('Internal server error');
   }
 });
+
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => {
