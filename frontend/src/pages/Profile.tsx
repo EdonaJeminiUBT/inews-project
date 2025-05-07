8f82027992f96b8c0d66b913ad4545af50a12cd0
ArbenitUBT <ab47053@ubt-uni.net>
2025-05-07 16:05:25 +0200

    Added the current user Profile

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,24 +1,40 @@
-export function Profile(){
+import { useEffect, useState } from 'react';
+import "./Profile.css"
 
-    return(
-        <div className="profile">
-            <div className="profile-header">
-            <img src="https://static.vecteezy.com/system/resources/previews/005/129/844/original/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg" width="200px"/>
-            <div className="email">
-        <div className="description-overview">
-                <button
-                  className="edit-name-btn"
-                >
-                  Change name
-                </button>
-              </div>
-       
-        <button className="signout-btn"
-            >
-              Sign out
-            </button>
+export function Profile() {
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    console.log('Token:', token);
+    fetch('http://localhost:3500/user-details', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUserName(data.username); 
+        setUserEmail(data.email);
+      })
+      .catch((error) => {
+        console.error('Error fetching user details:', error);
+      });
+  }, []);
+
+  return (
+    <div className="profile">
+      <div className="profile-header">
+        <img className="profile-img" src="https://static.vecteezy.com/system/resources/previews/005/129/844/original/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg" width="200px" alt="Profile" />
+        <div className="email">
+          <p className="welcome-text">Welcome, {userName}!</p>
+          <p className="email-text">Email: {userEmail}</p>
+          
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
