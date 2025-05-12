8bc83219720038561714023426c157fdd616457c
edonajeminiUBT <ej50024@ubt-uni.net>
2025-05-12 14:41:00 +0200

    Fixed some issues with the pages

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { FaFacebook, FaInstagram, FaTelegramPlane } from "react-icons/fa";
 
 export function Footer() {
   return (
@@ -5,9 +6,16 @@ export function Footer() {
       <div className="footer-content">
         <div className="footer-contact">
           <h2>Contact Us</h2>
-          <p>Email: INews@gmail.com</p>
-          <p>Phone: +383 111 11 111</p>
+          <p>INews@inews.com</p>
+          <p>+383 111 11 111</p>
         </div>
+      </div>
+      <div className="social-media">
+        <ul>
+          <li><FaFacebook /></li>
+          <li><FaInstagram /></li>
+          <li><FaTelegramPlane /></li>
+          </ul>
       </div>
       <div className="footer-bottom">
         <p>&copy; 2024 INews. All rights reserved.</p>
